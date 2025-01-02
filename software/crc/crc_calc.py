5316bdce8931395246a44a59830ac1914cc33b2a
Kevin Angstadt <kangstadt@stlawu.edu>
2025-01-02 18:46:07 -0500

    use a python program to calculate CRC

--- software/crc/crc_calc.py
+++ software/crc/crc_calc.py
@@ -1,8 +1,12 @@
 # simulate calucate crc value from input data
 
-import sys
+def little_endian(crc):
+    return f"0x{crc & 0xFF:02X}{crc >> 8:02X}"
 
-def crc16(data, num_bytes):
+def big_endian(crc):
+    return f"0x{crc:04X}"
+
+def crc16(data, num_bytes, all=False, little_endian=False):
     crc = 0x0000
     for i in range(num_bytes):
       crc ^= data[i]
@@ -11,26 +15,51 @@ def crc16(data, num_bytes):
           crc = (crc >> 1) ^ 0xA001
         else:
           crc >>= 1
+      if all:
+        if little_endian:
+          print(little_endian(crc))
+        else:
+          print(big_endian(crc))
     return crc
 
 def main():
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description='Calculate CRC16 of input data')
+    parser.add_argument('file', help='File to read data from')
+    parser.add_argument('num_bytes', nargs='?', type=int, help='Number of bytes to read from file')
+    parser.add_argument('--all', '-a', action='store_true', help='Print all CRC values up to the last byte')
+    parser.add_argument('--little-endian', '-l', action='store_true', help='Print CRC value in little endian')
+    parser.add_argument('--no-newline', '-n', action='store_true', help='Do not print newline at the end of the output')
+
+    args = parser.parse_args()
+
+
     # read data from file in first argument
     data = []
-    with open(sys.argv[1], 'rb') as f:
+    with open(args.file, 'rb') as f:
         data = f.read()
 
     # read number of bytes in second argument
     # if second argument does not exist, use all data
-    if len(sys.argv) < 3:
-        # last 2 bytes are crc value
-        num_bytes = len(data) - 2
+    if args.num_bytes is None:
+        num_bytes = len(data)
     else:
-      num_bytes = int(sys.argv[2])
+      num_bytes = args.num_bytes
 
-    crc = crc16(data, num_bytes)
+    crc = crc16(data, num_bytes, all=args.all, little_endian=args.little_endian)
     
-    # print as hex
-    print(hex(crc))
+    if not args.all:
+      if args.little_endian:
+        # print as little endian
+        print(little_endian(crc), end='')
+      else:
+        # print as hex
+        print(big_endian(crc), end='')
+      
+      if not args.no_newline:
+        print()
 
 if __name__ == '__main__':
     main()
